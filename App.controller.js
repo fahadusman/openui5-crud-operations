e29702ce9221f462548bf28fd1d19160a2b2bf4c
Fahad Usman <fahad.usman@gmail.com>
2018-02-23 09:56:34 +0500

    crud-app: Initial commit
    Signed-off-by: Fahad Usman <fahad.usman@gmail.com>

---
+++ App.controller.js
@@ -0,0 +1,184 @@
+﻿sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, JSONModel) {
+    "use strict";
+
+    return Controller.extend("crud.App", {
+
+        onInit: function () {
+            var oBusinessPartnerTable = sap.ui.getCore().byId("collectionTableID");
+            var oModel = oBusinessPartnerTable.getModel();
+            var FilterOperator = sap.ui.model.FilterOperator;
+
+            this.getView().setModel(oModel);
+        },
+
+        UpdateSelectedProduct: function () {
+            var oProductsTable = sap.ui.getCore().getElementById('ProductsTableID');
+            var selectedIndex = oProductsTable.getSelectedIndex();
+            if (selectedIndex == -1) {
+                alert("Please Select a row from Product Table to Update");
+                return;
+            }
+
+            var oModel = oProductsTable.getModel();
+
+            var selectedRow = oProductsTable.getRows()[selectedIndex];
+            var selectedId = selectedRow.getCells()[0].getValue();
+
+            var oMeta = oModel.getServiceMetadata();
+            var objectDeclaratoin = "";
+
+            var addComma = false;
+            for (var i = 0; i < oMeta.dataServices.schema[0].entityType[1].property.length; i++) {
+                var property = oMeta.dataServices.schema[0].entityType[1].property[i];
+                if(addComma) objectDeclaratoin += ",";
+                objectDeclaratoin += property.name + ":\"" + selectedRow.getCells()[i].getValue() + "\"";
+                addComma = true;
+            }
+            var oData;
+            eval('oData = {' + objectDeclaratoin + '};');
+
+            oModel.update("/ProductSet('" + selectedId + "')", oData, {
+                merge: true,
+                success: function () {
+                    alert("Product updated successfully.");
+                },
+                error: function (oError) {
+                    alert("failed to udpate the product.");
+                }
+            });
+        },
+
+        UpdateProductDialog: function () {
+            var oProductsTable = sap.ui.getCore().getElementById('ProductsTableID');
+            var selectedIndex = oProductsTable.getSelectedIndex();
+            if (selectedIndex == -1) {
+                alert("Please Select a row from Product Table to Update");
+                return;
+            }
+
+            var oModel = oProductsTable.getModel();
+
+            var selectedRow = oProductsTable.getRows()[selectedIndex];
+            var selectedId = selectedRow.getCells()[0].getValue();
+
+            var oMeta = oModel.getServiceMetadata();
+
+            //create dialog box for editing product details.
+            var oProductDetailsDialog = new sap.m.Dialog("DialogProductDetails", {
+                title: "Update Product Info",
+                modal: true,
+                contentWidth: "2em",
+                closed: function (oControlEvent) {
+                    sap.ui.getCore().getElementById('DialogProductDetails').destroy();
+                },
+                content: [
+                    new sap.m.Button("btnProductDetailsCancel", {
+                        text: "Cancel",
+                        tap: [this.Cancel, this]
+                    }),
+                    new sap.m.Button("btnProductDetailsUpdate", {
+                        text: "Update",
+                        tap: [this.UpdateProductData, this]
+                    }),
+                ]
+            });
+
+            var oProductDetailsList = new sap.m.List({
+                id: "ListProductDetails",
+                title: "Product Details",
+            });
+            oProductDetailsList.setModel(oModel);
+
+            var objectDeclaratoin = "";
+            var addComma = false;
+            for (var i = 0; i < oMeta.dataServices.schema[0].entityType[1].property.length; i++) {
+                var property = oMeta.dataServices.schema[0].entityType[1].property[i];
+                if (addComma) objectDeclaratoin += ",";
+                objectDeclaratoin += property.name + ":\"" + selectedRow.getCells()[i].getValue() + "\"";
+                addComma = true;
+
+                var oInput = new sap.m.Input({
+                    value: selectedRow.getCells()[i].getValue(),
+                    width: "150px",
+                    editable: (i == 0 || i == 2) ? false : true //Preserve productId and Category
+                });
+                oProductDetailsList.addItem(new sap.m.InputListItem({
+                    label: property.name,
+                    content: oInput,
+                }));
+            }
+            var oData;
+            eval('oData = {' + objectDeclaratoin + '};');
+
+            oProductDetailsDialog.addContent(oProductDetailsList);
+            oProductDetailsDialog.open();
+
+            oProductDetailsDialog.open();
+        },
+        UpdateProductData: function () {
+            var oProductsTable = sap.ui.getCore().getElementById('ProductsTableID');
+            var selectedIndex = oProductsTable.getSelectedIndex();
+            var oModel = oProductsTable.getModel();
+
+            var selectedRow = oProductsTable.getRows()[selectedIndex];
+            var selectedId = selectedRow.getCells()[0].getValue();
+
+            var oProductsDetailsDialog = sap.ui.getCore().getElementById('DialogProductDetails');
+            var oProductsDetailsList = sap.ui.getCore().getElementById('ListProductDetails');
+
+            var oMeta = oModel.getServiceMetadata();
+            var objectDeclaratoin = "";
+            var addComma = false;
+            for (var i = 0; i < oMeta.dataServices.schema[0].entityType[1].property.length; i++) {
+                var property = oMeta.dataServices.schema[0].entityType[1].property[i];
+                if (addComma) objectDeclaratoin += ",";
+                objectDeclaratoin += property.name + ":\"" +
+                    oProductsDetailsList.getItems()[i].getContent()[0].getValue() + "\"";
+                addComma = true;
+            }
+            var oData;
+            eval('oData = {' + objectDeclaratoin + '};');
+
+
+            oModel.update("/ProductSet('" + selectedId + "')", oData, {
+                merge: true,
+                success: function () {
+                    alert("Successfully updated the Product.");
+                    oProductsDetailsDialog.destroy();
+                },
+                error: function (oError) {
+                    alert("Failed to udpate.");
+                }
+            });
+
+        },
+
+        PartnerDetails: function () {
+            var partnerId = sap.ui.getCore().byId("inputPartnerId");
+            var sPartnerId = partnerId.getValue();
+            var oBusinessPartnerTable = sap.ui.getCore().byId("collectionTableID");
+
+            // Filter the DATA
+            var oModel = oBusinessPartnerTable.getModel();
+            var FilterOperator = sap.ui.model.FilterOperator;
+            console.log(sPartnerId);
+            var filter = new sap.ui.model.Filter("BusinessPartnerID", FilterOperator.EQ, sPartnerId);
+
+            //Bind the Data to the Table
+            oBusinessPartnerTable.bindRows("/BusinessPartnerSet", null, null, [filter]);
+
+            var oProductsTable = sap.ui.getCore().byId("ProductsTableID");
+            oProductsTable.bindRows("/BusinessPartnerSet('" + sPartnerId + "')/ToProducts", null, null, null);
+        },
+
+        Cancel: function () {
+
+            sap.ui.getCore().byId("DialogProductDetails").destroy();
+
+        }
+    });
+});
---
+++ App.view.js
@@ -0,0 +1,139 @@
+﻿sap.ui.jsview("crud.App", {
+    getControllerName: function () {
+        return "crud.App";
+    },
+
+    createContent: function (oController) {
+        var sServiceUrl = "https://sapes4.sapdevcenter.com/sap/opu/odata/IWBEP/GWSAMPLE_BASIC";
+        //        var oModel = new sap.ui.model.odata.v2.ODataModel({
+        var oModel = new sap.ui.model.odata.ODataModel({
+            serviceUrl: sServiceUrl,
+        });
+
+        var layout = new sap.ui.commons.layout.MatrixLayout({
+            id: 'matrix4',
+            layoutFixed: false,
+            width: "100%"
+        });
+
+        var appHeader = new sap.ui.commons.ApplicationHeader('appHeader', {
+            logoText: "CRUD Application",
+            displayLogoff: false,
+            displayWelcome: true,
+            userName: "User"
+        });
+
+        layout.createRow(appHeader);
+
+        var rPannel = new sap.ui.commons.Panel('rPannel', {
+            text: "CRUD Collection List",
+        });
+
+        var oMainToolbar = new sap.m.Toolbar({
+            content: [
+                new sap.m.Input("inputPartnerId", {
+                    width: "200px",
+                    value: "0100000001",
+                }),
+                new sap.m.Button({
+                    text: "Partner Details",
+                    press: function () {
+                        oController.PartnerDetails();
+                    }
+                }),
+
+                new sap.m.Button({
+                    text: "Update Product",
+                    press: function () {
+                        oController.UpdateSelectedProduct();
+                    }
+                }),
+                new sap.m.Button({
+                    text: "Update Product Dialog",
+                    press: function () {
+                        oController.UpdateProductDialog();
+                    }
+                }),
+            ],
+        });
+
+        layout.createRow(oMainToolbar);
+
+        // instantiate the table
+        var oBusinessPartnerTable = new sap.ui.table.Table({
+            id: "collectionTableID",
+            title: "Business Partners",
+            visibleRowCount: 1,
+            editable: false,
+            rowSelectionChange: function (oEvent) { },
+        });
+
+        oBusinessPartnerTable.setModel(oModel);
+
+        var oMeta = oModel.getServiceMetadata();
+        if (oMeta == undefined) {
+            alert("metadata is not defined yet");
+        }
+
+        for (var i = 1; i < oMeta.dataServices.schema[0].entityType[0].property.length; i++) {
+            var property = oMeta.dataServices.schema[0].entityType[0].property[i];
+
+            oControl = new sap.ui.commons.TextField({ editable: false }).bindProperty("value", property.name);
+            oBusinessPartnerTable.addColumn(new sap.ui.table.Column({
+                label: new sap.ui.commons.Label({ text: property.name }),
+                template: oControl, sortProperty: property.name, filterProperty: property.name,
+                filterOperator: sap.ui.model.FilterOperator.EQ, flexible: true, width: "125px"
+            }));
+        }		   	  
+
+        layout.createRow(oBusinessPartnerTable);
+
+        // Add table to the Panel
+        rPannel.addContent(oBusinessPartnerTable);
+
+        var oPartnerProductsTable = new sap.ui.table.Table({
+            id: "ProductsTableID",
+            title: "Products",
+            visibleRowCount: 6,
+            editable: false,
+            rowSelectionChange: function (oEvent) { },
+        });
+
+        oPartnerProductsTable.setModel(oModel);
+
+        var oMeta = oModel.getServiceMetadata();
+        if (oMeta == undefined) {
+            alert("metadata is not defined yet");
+        }
+
+        for (var i = 0; i < oMeta.dataServices.schema[0].entityType[1].property.length; i++) {
+            var property = oMeta.dataServices.schema[0].entityType[1].property[i];
+
+            oControl = new sap.ui.commons.TextField({ editable: (i==0 || i==2)?false:true }); //ID and category fields are not editable.
+            oControl.bindProperty("value", property.name);
+            oPartnerProductsTable.addColumn(new sap.ui.table.Column({
+                label: new sap.ui.commons.Label({ text: property.name }),
+                template: oControl,
+                sortProperty: property.name,
+                filterProperty: property.name,
+                filterOperator: sap.ui.model.FilterOperator.EQ,
+                flexible: true,
+                width: "125px"
+            }));
+
+
+        }
+
+        layout.createRow(oPartnerProductsTable);
+
+        // Add table to the Panel
+        rPannel.addContent(oPartnerProductsTable);
+
+        // Add panel to the Layout
+        layout.createRow(rPannel);
+        // Display Layout
+        this.addContent(layout);
+
+        return layout;
+    }
+});
